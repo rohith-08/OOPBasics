dcfcc02c3817fd02702911118594c3b94e583665
Rohith <rohithneela369@gmail.com>
2026-03-27 22:13:36 +0530

    Add project files.

---
+++ OOPBasics/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace OOPBasics
+{
+    internal class Animal
+    {
+        public string Name { get; set; }//encapsulation
+        public virtual void MakeSound()
+        {
+            Console.WriteLine($"makes the sound {Name}");//polymorphism
+
+        }
+    }
+ internal   class Dog : Animal
+    {
+
+        public override void MakeSound()
+        {
+            Console.WriteLine($"{Name} Makes sound like bow!");
+        }
+
+    }
+    internal class Cat : Animal
+    {
+
+        public override void MakeSound()
+        {
+            Console.WriteLine($"{Name} Makes sound like Meow!");
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal Dog = new Dog {Name="Dog" };
+            Animal Cat = new Cat { Name = "Cat" };
+            Dog.MakeSound();
+            Cat.MakeSound();
+
+            Console.WriteLine("Day1 -Completed!!");
+        }
+    }
+}
